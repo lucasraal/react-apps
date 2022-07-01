97cc03d44d486174740d68c748084a77fc27b1a5
lucasraal <lucasraal@gmail.com>
2022-06-30 22:54:19 -0300

    s11 145

--- 05-food/src/App.js
+++ 05-food/src/App.js
@@ -1,5 +1,6 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
+import CartProvider from "./store/CartProvider";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
@@ -15,13 +16,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
